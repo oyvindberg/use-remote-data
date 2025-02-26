d03191959348219a6c88cf5a1a73861a08d44e88
Øyvind Raddum Berg <elacin@gmail.com>
2025-02-26 07:46:07 +0100

    style

--- src/internal/isDefined.ts
+++ src/internal/isDefined.ts
@@ -1,4 +1,2 @@
-type DefinedTypes = object | number | boolean | symbol | string;
-
 // this exists for type inference
-export const isDefined = <T extends DefinedTypes>(t: T | null | undefined): t is T => t !== null && t !== void 0;
+export const isDefined = <T>(t: T | null | undefined): t is T => t !== null && t !== void 0;
